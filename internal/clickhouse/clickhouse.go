d745d72926c1b33dcbddc2e2a53dc31cfbc5351c
M Samuel Vijaykumar <mad.vijay@gmail.com>
2025-09-09 17:51:36 +0530

    refactor: Remove unused global stats and ClickHouse hooks implementation

--- internal/clickhouse/clickhouse.go
+++ internal/clickhouse/clickhouse.go
@@ -3,24 +3,12 @@ package clickhouse
 import (
 	"context"
 	"database/sql"
-	"errors"
-	"sync"
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/freshworks/load-generator/internal/stats"
 	"github.com/sirupsen/logrus"
 )
-
-// TODO: Find a way to make it not global!
-var gStats *stats.Stats
-var gStatsMux sync.Mutex
-
-func init() {
-	// Register ClickHouse driver with hooks
-	// For now, we'll use a simpler approach without sqlhooks
-	// TODO: Implement proper driver wrapping for ClickHouse
-}
 
 type Generator struct {
 	DB *sql.DB
@@ -44,11 +32,6 @@ func NewOptions() *GeneratorOptions {
 }
 
 func NewGenerator(id int, options GeneratorOptions, ctx context.Context, requestrate int, s *stats.Stats) *Generator {
-	gStatsMux.Lock()
-	defer gStatsMux.Unlock()
-
-	gStats = s
-
 	log := logrus.WithFields(logrus.Fields{"Id": id})
 	return &Generator{log: log, o: options, ctx: ctx, stats: s}
 }
@@ -108,40 +91,4 @@ func (g *Generator) Open(dsn string) (*sql.DB, error) {
 	}
 
 	return clickhouse.OpenDB(opt), nil
-}
-
-type Hooks struct{}
-type begincontext string
-
-var begin = begincontext("begin")
-
-// Before hook will print the query with it's args and return the context with the timestamp
-func (h *Hooks) Before(ctx context.Context, query string, args ...interface{}) (context.Context, error) {
-	return context.WithValue(ctx, begin, time.Now()), nil
-}
-
-// After hook will get the timestamp registered on the Before hook and print the elapsed time
-func (h *Hooks) After(ctx context.Context, query string, args ...interface{}) (context.Context, error) {
-	var traceInfo stats.TraceInfo
-	traceInfo.Type = stats.ClickHouseTrace
-	traceInfo.Key = "" // TODO: Set it host
-	traceInfo.Subkey = query
-	traceInfo.Total = time.Since(ctx.Value(begin).(time.Time))
-	gStats.RecordMetric(&traceInfo)
-
-	return ctx, nil
-}
-
-func (h *Hooks) OnError(ctx context.Context, err error, query string, args ...interface{}) error {
-	var traceInfo stats.TraceInfo
-	traceInfo.Type = stats.ClickHouseTrace
-	traceInfo.Key = "" // TODO: Set it host
-	traceInfo.Subkey = query
-
-	if !errors.Is(err, context.Canceled) {
-		traceInfo.Error = true
-	}
-	gStats.RecordMetric(&traceInfo)
-
-	return err
 }
